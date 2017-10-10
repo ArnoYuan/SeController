9a87a9c21dbd274f02ff97827a70d71e77a7188d
cyberniklee <cyberniklee@gmail.com>
2017-10-10 05:39:37 -0700

    update application luncher

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,23 +6,55 @@
  */
 
 #include "ControllerApplication.h"
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 using namespace NS_Controller;
+
+ControllerApplication* app;
+
+static void
+signalAction (int signal)
+{
+  printf ("received term signal, quitting!\n");
+  app->quit ();
+  app->terminate ();
+}
+
+void
+registerSignal ()
+{
+	//signal (SIGINT, signalAction);
+	//signal (SIGKILL, signalAction);
+	//signal (SIGQUIT, signalAction);
+	//signal (SIGTERM, signalAction);
+	signal (SIGUSR1, signalAction);
+}
 
 int
 main (int argc, char* argv[])
 {
-  ControllerApplication app;
-  
-  if (!app.initialize (argc, argv))
+  app = new ControllerApplication;
+
+  registerSignal ();
+
+  if (!app->initialize (argc, argv))
   {
-	return -1;
+	exit (EXIT_FAILURE);
+	return 0;
   }
   
-  app.run ();
+  app->run ();
   
-  app.pending ();
+  if (!app->isRunning())
+  {
+	exit (EXIT_FAILURE);
+	return 0;
+  }
+
+  app->pending ();
   
+  exit (EXIT_SUCCESS);
   return 0;
 }
-
